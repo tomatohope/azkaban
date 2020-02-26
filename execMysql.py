13560028ac01492a73f2efdb6854e7d6e0400dd1
hope.gong <hope.gong@jingdigital.com>
2020-02-26 21:49:58 +0800

    fix

--- execMysql.py
+++ execMysql.py
@@ -2,4 +2,4 @@
 from connectMysql import execsql
 import sys
 
-execsql.execsqlfile(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], '0')
+execsql.execsqlfile(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], '0', sys.argv[6])
--- execPgsql.py
+++ execPgsql.py
@@ -2,4 +2,4 @@
 from connectPgsql import execsql
 import sys
 
-execsql.execsqlfile(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], '0')
+execsql.execsqlfile(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], '0', sys.argv[7])
