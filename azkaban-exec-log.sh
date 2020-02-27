47b5e687fbcf1023cea1d0f9237579d835b0bdf5
tomatohope <45392995+tomatohope@users.noreply.github.com>
2020-02-28 04:06:08 +0800

    Delete azkaban-exec-log.sh

--- azkaban-exec-log.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -f /opt/services/azkaban/azkaban-exec-server/build/install/azkaban-exec-server/bin/executorServerLog*
-rm -f /opt/services/azkaban/azkaban-exec-server/build/install/azkaban-exec-server/bin/logs/*.log
